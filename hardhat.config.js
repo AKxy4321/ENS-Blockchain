a5c1fcc71a2fddf33ab0aaf789fc0a0eed37b067
Aditya <adityakushal23@gmail.com>
2023-11-18 16:57:11 +0530

    o

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
-require("@nomicfoundation/hardhat-ethers");
-require("@nomicfoundation/hardhat-chai-matchers");
+require('@nomicfoundation/hardhat-chai-matchers');
+require('@nomicfoundation/hardhat-ethers');
+require('ethers');
 
 module.exports = {
   solidity: "0.8.20",
